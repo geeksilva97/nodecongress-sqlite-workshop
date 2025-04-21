eef05116a0227c5558aff4c1804e030652d44e51
Edy Silva <edigleyssonsilva@gmail.com>
2025-04-21 12:20:47 -0300

    challenge 4 solved

--- 04-user-defined-functions/index.js
+++ 04-user-defined-functions/index.js
@@ -2,5 +2,7 @@ import { DatabaseSync } from 'node:sqlite';
 
 export const conn = new DatabaseSync(':memory:');
 
-conn.function('myconcat', () => ``);
-conn.function('myconcat2', () => {});
+conn.function('myconcat', (a, b) => `${a}${b}`);
+conn.function('myconcat2', {
+  varargs: true
+}, (separator, ...args) => `${args.join(separator)}`);
