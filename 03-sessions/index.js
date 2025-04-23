d3816954e070f548a1e71e6bec607a0ab591057a
Edy Silva <edigleyssonsilva@gmail.com>
2025-04-23 13:53:19 -0300

    fix sessions sample

--- 03-sessions/index.js
+++ 03-sessions/index.js
@@ -1,4 +1,8 @@
 function executeQueries(conn) {
+  conn.exec(`
+    INSERT INTO users (name, email) VALUES ('Mark Grayson', 'mark@viltrum.com'),
+                                          ('Nolan Grayson', 'nolan@viltrum.com');
+  `);
 }
 
 export function getChangeset(conn) {
